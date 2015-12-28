e98b390bf91b61b77b642f49090d96132641d520
alex <alexrussinov@gmail.com>
2015-12-28 21:42:47 +0300

    refactor roundRobin

--- src/main/scala/Aggregator.scala
+++ src/main/scala/Aggregator.scala
@@ -1,6 +1,7 @@
-import play.api.libs.iteratee.Enumerator
+import play.api.libs.iteratee.{Iteratee, Enumerator}
 import utils.CommonUtilsImplicits.FutureExtended
 
+import scala.annotation.tailrec
 import scala.concurrent.{Future}
 import scala.util.{Try, Failure, Success}
 
@@ -8,12 +9,23 @@ import scala.util.{Try, Failure, Success}
   * Created by Aleksey Voronets on 28.12.15.
   */
 trait Aggregator extends SearchTypes {
-    def parser()
     def aggregateSearch(searchPhrase: String, searchEngines: Seq[SearchEngine], aggregationStrategy: AggregationStrategy): Future[Seq[Try[(SearchEngine, SearchResult)]]]
 }
 
 object Aggregator
 {
+
+    def mergeResults[T](results: Seq[Seq[T]]): Seq[T] = {
+        @tailrec
+        def step(results: Seq[Seq[T]], acc: Seq[T]): Seq[T] = {
+            if (results.forall(_.isEmpty)) acc
+            else step(results.withFilter(_.nonEmpty).map(_.tail), acc ++ results.flatMap(_.headOption))
+        }
+        step(results, Nil)
+    }
+
+
+
     def roundRobin(searchEngines: (SearchEngine, SearchEngine), searchPhrase: String): Future[Seq[String]] = {
         val engine1 = searchEngines._1
         val engine2 = searchEngines._2
@@ -25,13 +37,15 @@ object Aggregator
         } yield (result1.zip(result2).foldRight(Seq[String]())((a, acc) => (acc :+ a._1) :+ a._2))
     }
 
-    def interleavingStrategy(searchEngines: Seq[SearchEngine], searchPhrase: String) = {
-        val enumerators = searchEngines.map(_.search(searchPhrase)).map(futureResult => Enumerator.flatten(futureResult.map(Enumerator(_))))
-        val s = Enumerator.interleave(enumerators).run()
+    /** FIFO web service that responds first appear first in the result set **/
+    def interleavingStrategy(searchEngines: Seq[SearchEngine], searchPhrase: String): Future[Seq[String]] = {
+        val enumerators = searchEngines.map(engine => engine.search(searchPhrase).map(engine.parse))
+            .map(futureResult => Enumerator.flatten(futureResult.map(Enumerator(_))))
+        Enumerator.interleave(enumerators).run(Iteratee.fold(Seq[String]()){(acc, el) => acc ++ el})
     }
+
     def apply(): Aggregator = {
         new Aggregator {
-            def parser() = ???
             def aggregateSearch(searchPhrase: String, searchEngines: Seq[SearchEngine], aggregationStrategy: AggregationStrategy): Future[Seq[Try[(SearchEngine, SearchResult)]]] =
             {
                 val searchResultsTry: Seq[Future[Try[(SearchEngine, SearchResult)]]] =
