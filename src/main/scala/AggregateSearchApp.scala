aad764d2903bae18978a304c7f4d1db372823c41
alex <avoronets@naumen.ru>
2015-12-29 14:18:10 +0300

    *formatting

--- src/main/scala/AggregateSearchApp.scala
+++ src/main/scala/AggregateSearchApp.scala
@@ -1,6 +1,6 @@
 import scala.concurrent.ExecutionContext.Implicits.global
 
-object AggregateSearchApp extends App{
+object AggregateSearchApp extends App {
     val aggregator = Aggregator()
     val engines = Seq(new BingSearch("dZHx2LeVZb9BHorIDiwiIPTetFDaeqOgBHBJAScDzFE"),
         new GoogleSearch("AIzaSyDzkcGGhgkB4jjY22OavyJuyfuNf5AuMTg", "008366210748654328620:cicfenuzhr0"))
--- src/test/scala/AggregateTestSpec.scala
+++ src/test/scala/AggregateTestSpec.scala
@@ -7,18 +7,15 @@ import Matchers._
 import scala.concurrent.ExecutionContext.Implicits.global
 
 
-class AggregateTestSpec extends FunSuite with ScalaFutures{
-    implicit val defaultPatience =
-        PatienceConfig(timeout = Span(10, Seconds), interval = Span(500, Millis))
+class AggregateTestSpec extends FunSuite with ScalaFutures {
+    implicit val defaultPatience = PatienceConfig(timeout = Span(10, Seconds), interval = Span(500, Millis))
 
     test("Aggregate search with DummyEngines - round robin strategy"){
         val aggregator = Aggregator()
         val engines = Seq(new DummyEngine1Impl, new DummyEngine2Impl)
         val result = aggregator.aggregateSearch("test phrase", engines, aggregator.roundRobin)
 
-        whenReady(result){ r =>
-            r should contain inOrder("A", "D", "B", "E", "C", "F")
-        }
+        whenReady(result){ _ should contain inOrder("A", "D", "B", "E", "C", "F") }
     }
 
     test("Aggregate search with DummyEngines - fifo strategy"){
@@ -26,27 +23,25 @@ class AggregateTestSpec extends FunSuite with ScalaFutures{
         val engines = Seq(new DummyEngine1Impl, new DummyEngine2Impl)
         val result = aggregator.aggregateSearch("test phrase", engines, aggregator.fifoStrategy)
 
-        whenReady(result){ r =>
-            r should contain inOrder("D", "E", "F", "A", "B", "C")
-        }
+        whenReady(result){ _ should contain inOrder("D", "E", "F", "A", "B", "C") }
     }
 }
 
-trait DummyEngine extends SearchEngine{
+trait DummyEngine extends SearchEngine {
 
     override def client() = new { def close() = {} }
 
     override def baseUrl: String = "url"
 }
 
-class DummyEngine1Impl extends DummyEngine{
+class DummyEngine1Impl extends DummyEngine {
 
     override def search(searchPhrase: String): Future[SearchResult] = Future{ Thread.sleep(1000); JsNull }
 
     override def parse(searchResult: SearchResult): Seq[String] = Seq("A", "B", "C")
 }
 
-class DummyEngine2Impl extends DummyEngine{
+class DummyEngine2Impl extends DummyEngine {
 
     override def search(searchPhrase: String): Future[SearchResult] = Future{ Thread.sleep(500); JsNull }
 
